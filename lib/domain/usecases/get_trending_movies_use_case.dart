ede3f1930f947b193a31421cb832bf22b55d1559
LindseyFontana <lindseyfontanas@gmail.com>
2026-08-01 23:04:13 -0300

    refactor: class useCase

--- lib/domain/usecases/get_trending_movies_use_case.dart
+++ lib/domain/usecases/get_trending_movies_use_case.dart
@@ -2,13 +2,7 @@ import 'package:movie_search/domain/entities/trending_movies.dart';
 import 'package:movie_search/domain/repository/movies_repository.dart';
 import 'package:movie_search/domain/usecases/use_case.dart';
 
-class GetTrendingMoviesUseCase implements UseCase<TrendingMovies, void> {
-  final MoviesRepository _repository;
-
-  const GetTrendingMoviesUseCase(this._repository);
-
-  @override
-  Future<TrendingMovies> call(void params) async {
-    return _repository.getTrendingMovies();
-  }
+class GetTrendingMoviesUseCase extends UseCase<TrendingMovies, void> {
+  GetTrendingMoviesUseCase(MoviesRepository repository)
+    : super(request: (_) => repository.getTrendingMovies());
 }
