6ba28c1d6d85a1e34659b10c087c4c3e33879848
LindseyFontana <lindseyfontanas@gmail.com>
2026-08-04 19:08:37 -0300

    refactor: removes unnecesary code

--- lib/presentation/search_movies/bloc/search_movies_bloc.dart
+++ lib/presentation/search_movies/bloc/search_movies_bloc.dart
@@ -1,7 +1,5 @@
 import 'package:bloc_concurrency/bloc_concurrency.dart';
-import 'package:either_dart/either.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_search/domain/entities/movie.dart';
 import 'package:movie_search/domain/entities/trending_movies.dart';
 import 'package:movie_search/domain/usecases/get_trending_movies_use_case.dart';
 part 'search_movies_event.dart';
@@ -23,23 +21,6 @@ class SearchMoviesBloc extends Bloc<MoviesEvent, SearchMoviesState> {
         (trendingMovies) => emit(SuccessState(trendingMovies: trendingMovies)),
       );
     });
-
-    // on<LoadMoreTrendingMoviesEvent>((event, emit) async {
-    //   emit(LoadingMoreMoviesState(trendingMovies: event.trendingMovies));
-
-    //   final result = await _fetchNextTrendingPage(event.trendingMovies);
-
-    //   result.fold((error) => emit(ErrorState()), (nextPageTrendingMovies) {
-    //     emit(
-    //       SuccessState(
-    //         trendingMovies: _getTrendingMoviesUpdated(
-    //           movies: event.trendingMovies?.movies,
-    //           nextTrendingMovies: nextPageTrendingMovies,
-    //         ),
-    //       ),
-    //     );
-    //   });
-    // }, transformer: droppable());
 
     on<LoadMoreTrendingMoviesEvent>((event, emit) async {
       emit(LoadingMoreMoviesState(trendingMovies: event.trendingMovies));
