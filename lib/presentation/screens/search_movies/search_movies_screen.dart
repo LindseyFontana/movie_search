6ff733da90d9c886f98d3c782916152ee290ac76
LindseyFontana <lindseyfontanas@gmail.com>
2026-08-05 15:41:09 -0300

    refactor: bloc rebuild onlyt necessary widgets

--- lib/presentation/screens/search_movies/search_movies_screen.dart
+++ lib/presentation/screens/search_movies/search_movies_screen.dart
@@ -33,56 +33,115 @@ class _SearchMoviesState extends State<SearchMoviesScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Center(child: Text('Filmes'))),
-        body: BlocBuilder<SearchMoviesBloc, SearchMoviesState>(
-          bloc: bloc,
-          builder: (context, state) {
-            final paginetedMovies = state.paginetedMovies;
-
-            if (state is SuccessState || state is LoadingMoreMoviesState) {
-              return Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _inputTextController,
-                      decoration: InputDecoration(
-                        labelText: 'Pesquisar',
-                        hintText: 'Digite o título do filme',
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.clear),
-                          onPressed: () {
-                            _inputTextController.text = "";
-                            //TODO: salvar a primeira página em cache, talvez atualizar
-                            //diariamente ou mensalmente para não perder a atualizadade da listagem
-                            bloc.add(GetTrendingMoviesEvent());
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      onSubmitted: (query) =>
-                          bloc.add(SearchMoviesEvent(query)),
-                    ),
-
-                    SizedBox(height: 32),
-
-                    if (paginetedMovies != null &&
-                        paginetedMovies.movies.isNotEmpty)
-                      EndlessScrolling(paginatedMovies: paginetedMovies),
-                    if (state is LoadingMoreMoviesState)
-                      CircularProgressIndicator(),
-                  ],
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                controller: _inputTextController,
+                decoration: InputDecoration(
+                  labelText: 'Pesquisar',
+                  hintText: 'Digite o título do filme',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      _inputTextController.text = "";
+                      //TODO: salvar a primeira página em cache, talvez atualizar
+                      //diariamente ou mensalmente para não perder a atualizadade da listagem
+                      bloc.add(GetTrendingMoviesEvent());
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
-              );
-            }
-            if (state is ErrorState) {
-              return Center(child: Text("Error"));
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },
+                onSubmitted: (query) => bloc.add(SearchMoviesEvent(query)),
+              ),
+
+              SizedBox(height: 32),
+
+              BlocBuilder<SearchMoviesBloc, SearchMoviesState>(
+                bloc: bloc,
+                builder: (context, state) {
+                  final paginetedMovies = state.paginetedMovies;
+
+                  if (state is SuccessState ||
+                      state is LoadingMoreMoviesState) {
+                    return Expanded(
+                      child: Column(
+                        children: [
+                          if (paginetedMovies != null &&
+                              paginetedMovies.movies.isNotEmpty)
+                            EndlessScrolling(paginatedMovies: paginetedMovies),
+                          if (state is LoadingMoreMoviesState)
+                            CircularProgressIndicator(),
+                        ],
+                      ),
+                    );
+                  }
+                  if (state is ErrorState) {
+                    return Center(child: Text("Error"));
+                  } else {
+                    return Expanded(
+                      child: Center(child: CircularProgressIndicator()),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
+
+        // BlocBuilder<SearchMoviesBloc, SearchMoviesState>(
+        //   bloc: bloc,
+        //   builder: (context, state) {
+        //     final paginetedMovies = state.paginetedMovies;
+
+        //     if (state is SuccessState || state is LoadingMoreMoviesState) {
+        //       return Padding(
+        //         padding: EdgeInsets.all(16),
+        //         child: Column(
+        //           children: [
+        //             TextField(
+        //               controller: _inputTextController,
+        //               decoration: InputDecoration(
+        //                 labelText: 'Pesquisar',
+        //                 hintText: 'Digite o título do filme',
+        //                 suffixIcon: IconButton(
+        //                   icon: Icon(Icons.clear),
+        //                   onPressed: () {
+        //                     _inputTextController.text = "";
+        //                     //TODO: salvar a primeira página em cache, talvez atualizar
+        //                     //diariamente ou mensalmente para não perder a atualizadade da listagem
+        //                     bloc.add(GetTrendingMoviesEvent());
+        //                   },
+        //                 ),
+        //                 border: OutlineInputBorder(
+        //                   borderRadius: BorderRadius.circular(8),
+        //                 ),
+        //               ),
+        //               onSubmitted: (query) =>
+        //                   bloc.add(SearchMoviesEvent(query)),
+        //             ),
+
+        //             SizedBox(height: 32),
+
+        //             if (paginetedMovies != null &&
+        //                 paginetedMovies.movies.isNotEmpty)
+        //               EndlessScrolling(paginatedMovies: paginetedMovies),
+        //             if (state is LoadingMoreMoviesState)
+        //               CircularProgressIndicator(),
+        //           ],
+        //         ),
+        //       );
+        //     }
+        //     if (state is ErrorState) {
+        //       return Center(child: Text("Error"));
+        //     } else {
+        //       return Center(child: CircularProgressIndicator());
+        //     }
+        //   },
+        // ),
       ),
     );
   }
