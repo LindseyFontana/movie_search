817647a588369a28dce88a0aa58ce59fa784ed9f
LindseyFontana <lindseyfontanas@gmail.com>
2026-08-07 15:09:23 -0300

    refactor: replace expanded with center

--- lib/presentation/screens/widgets/custom_error_widget.dart
+++ lib/presentation/screens/widgets/custom_error_widget.dart
@@ -9,39 +9,38 @@ class CustomErrorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: switch (error.type) {
-        ErrorType.connection => _buildError(
-          icon: Icons.signal_wifi_off,
-          title: AppStrings.errors.connectionTitle,
-          subtitle: AppStrings.errors.connectionSubtitle,
-        ),
-        ErrorType.api => _buildError(
-          title: AppStrings.errors.apiTitle,
-          subtitle: AppStrings.errors.apiSubtitle,
-        ),
-        _ => _buildError(title: AppStrings.errors.genericTitle),
-      },
-    );
+    return switch (error.type) {
+      ErrorType.connection => _buildError(
+        icon: Icons.signal_wifi_off,
+        title: AppStrings.errors.connectionTitle,
+        subtitle: AppStrings.errors.connectionSubtitle,
+      ),
+      ErrorType.api => _buildError(
+        title: AppStrings.errors.apiTitle,
+        subtitle: AppStrings.errors.apiSubtitle,
+      ),
+      _ => _buildError(title: AppStrings.errors.genericTitle),
+    };
   }
 
   Widget _buildError({
     IconData? icon,
     required String title,
     String? subtitle,
-  }) => Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Icon(
-        icon ?? Icons.error_outline,
-        size: 80,
-        color: const Color.fromRGBO(161, 45, 36, 1),
-      ),
-      SizedBox(height: AppSizes.spacing.small),
-      Text(title, style: TextStyle(fontSize: AppSizes.font.title)),
-      if (subtitle != null)
-        Text(subtitle, style: TextStyle(fontSize: AppSizes.font.subtitle)),
-    ],
+  }) => Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon ?? Icons.error_outline,
+          size: 80,
+          color: const Color.fromRGBO(161, 45, 36, 1),
+        ),
+        SizedBox(height: AppSizes.spacing.small),
+        Text(title, style: TextStyle(fontSize: AppSizes.font.title)),
+        if (subtitle != null)
+          Text(subtitle, style: TextStyle(fontSize: AppSizes.font.subtitle)),
+      ],
+    ),
   );
 }
